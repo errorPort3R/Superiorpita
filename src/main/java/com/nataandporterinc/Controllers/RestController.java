d1776716966b355dfb0f5e588560b57c1c5c9d3c
jeffryporter <jporter7@gmail.com>
2016-12-08 19:54:01 -0500

    Start Restcontroller

--- src/main/java/com/nataandporterinc/Controllers/RestController.java
+++ src/main/java/com/nataandporterinc/Controllers/RestController.java
@@ -1,9 +1,30 @@
 package com.nataandporterinc.Controllers;
 
+import com.nataandporterinc.services.CustomerRepository;
+import com.nataandporterinc.services.EmployeeRepository;
+import com.nataandporterinc.services.OrderRepository;
+import com.nataandporterinc.services.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 /**
  * Created by jeffryporter on 12/7/16.
  */
+
+@org.springframework.web.bind.annotation.RestController
 public class RestController
 {
+    @Autowired
+    CustomerRepository customers;
+
+    @Autowired
+    EmployeeRepository employees;
+
+    @Autowired
+    OrderRepository orders;
+
+    @Autowired
+    ProductRepository products;
+
+
 
 }
